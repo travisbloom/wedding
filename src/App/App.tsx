ba796d901ba8091fc75d6edcc21b714a85ee3c0c
Travis Bloom <travisbloom@gmail.com>
2018-10-14 19:38:56 -0400

    remove photos for now

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { HeaderVideo } from 'src/components/HeaderVideo/HeaderVideo';
-import { PhotoGalary } from 'src/components/PhotoGallery/PhotoGallery';
 export class App extends React.Component {
   render() {
     return (
       <div>
         <HeaderVideo />
-        <PhotoGalary />
       </div>
     );
   }
